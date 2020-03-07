1665604539f69ce38a71385a14f649159c1cc5e7
Nirajan Kharel <kharelnirajan@gmail.com>
2020-03-07 12:54:06 -0600

    fetched data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Cards from "./components/Cards"
+import Pages from "./components/Pages"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,10 +12,35 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+
+  const [charArr, setCharArr] = useState();
+  const [page, setPage] = useState(1);
+
+  useEffect( () => {
+    axios.get(`https://swapi.co/api/people/?&page=${page}`)
+      .then(res => {
+        console.log(res);
+        setCharArr(res.data.results);
+      })
+      .catch(err => console.log(err))
+
+  }, [page]);
+
+
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <div>
+        <Cards charArr = {charArr}/>
+      </div>
+      <div style ={{display: "flex", justifyContent: "center"}}>
+        <Pages setPage={setPage}/>
+      </div>
+      
     </div>
+
   );
 }
 
